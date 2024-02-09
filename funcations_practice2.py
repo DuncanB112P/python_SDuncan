a98b7e27a4dbe9246179f72a52a1d41b1c8ae9b0
DuncanB112P <samuel.p.duncan@gmail.com>
2024-02-09 18:44:01 +0000

    practice with strings, numbers, and functions in python

--- funcations_practice2.py
+++ funcations_practice2.py
@@ -1,11 +1,23 @@
 #Practice calling function within function as means of organizing
 # top to bottom code
 
+'''
 def main():
     x = input('response = ')
     print(convert(x))
 
 def convert(answer):
     return answer.replace(':)', '🙂').replace(':(', '🙁')
+
+main()
+
+'''
+
+def main():
+    x = tip(input('Meal cost: '))
+    print('Tip = ', x)
+
+def tip(d):
+    return float(d)
 
 main()
