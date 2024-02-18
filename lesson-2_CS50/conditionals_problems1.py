66b1a06960e523c3e06bfde2190f227f50054c23
Sam Duncan <samuel.p.duncan@gmail.com>
2024-02-18 14:00:28 -0500

    Pythin coding practive from Harvard CS50 course

---
+++ lesson-2_CS50/conditionals_problems1.py
@@ -0,0 +1,9 @@
+
+answer = input('What is the answer to the Great Question of Life, the Universe and Everything? ').lower().strip()
+print(answer) 
+
+match answer:
+    case '42' | 'forty-two' | 'forty two':
+        print('Yes')
+    case _:
+        print('No')
---
+++ lesson-2_CS50/loops_practice2.py
@@ -0,0 +1,35 @@
+students = ['Hermione', 'Harry', 'Ron']
+
+'''for student in students:
+    print(student)'''
+
+
+'''for i in range(len(students)):  #using 'len' to identify the length of the list for Python to iterate through
+    print(students[i])'''
+
+
+
+
+# DICTIONARIES
+    
+'''students = {
+    'Hermione': 'Griffyndor',
+    'Harry': 'Gryffindor',
+    'Draco': 'Slytherin',
+}
+
+for student in students:
+    print(student, students[student], sep=', ')'''
+
+
+# LIST OF DICTIONARIES
+
+students = [
+    {'name': 'Hermione', 'house': 'Griffyndor', 'patronus': 'Otter'},
+    {'name': 'Harry', 'house': 'Griffyndor', 'patronus': 'Stag'},
+    {'name': 'Ron', 'house': 'Griffyndor', 'patronus': 'Dog'},
+    {'name': 'Draco', 'house': 'Slytherin', 'patronus': 'None'}
+]
+
+for student in students:
+    print(student['name'], student['house'], student['patronus'], sep=', ')
