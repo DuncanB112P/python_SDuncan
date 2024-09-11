c2f62d46d9890ac498b8948579a977f88a3ec40c
Sam Duncan <samuel.p.duncan@gmail.com>
2024-09-11 00:44:35 -0400

    Python coding practice from Harvard CS50 course

---
+++ lesson-4_CS50/libraries_random.py
@@ -0,0 +1,37 @@
+# import the Random library
+import random
+
+#Create a variable coin and use the random.choice function to pass in the parameters heads and tails
+coin = random.choice(["heads", "tails"])
+print(coin)
+
+
+# You can also use "from" the module to place the scope of the library into the working file
+
+'''
+from random import choice
+coin = choice(["heads", "tails"])   # no need to use "random" in this method
+print(coin)
+'''
+#####################################################################################################
+
+
+# RANDINT -- get a random integer from a range
+
+import random
+
+number = random.randint(1,10)
+print(number)
+
+
+############################################################################################
+
+# SHUFFLE
+
+import random
+
+cards = ['J', 'Q', 'K', 'A', '10']
+random.shuffle(cards)
+for card in cards:
+    print(card)
+
